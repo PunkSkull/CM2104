3afc85c2f5d48fa41a9dad1692ecfa7d40acf2ea
1612673 <acquablue@live.it>
2018-03-13 14:02:23 +0000

    Lab13 part4

---
+++ lab13/mythirdserver.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var oneLinerJoke = require(‘one-liner-joke’);
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var randomJoke = oneLinerJoke.getRandomJoke();
+    //res.write("The date and time are currently: " + currentdate.myDateTime());
+    res.end(randomJoke.body);
+  }).listen(8080);
