910a7b97b0b7f8e329c305083f9983ccb6364519
1612673 <acquablue@live.it>
2018-03-13 15:06:00 +0000

    Lab13 part7

--- lab13/server.js
+++ lab13/server.js
@@ -1,9 +1,17 @@
 var express = require('express');
+  app.use(express.static('public'))
 var app = express();
-app.get('/', function(req, res){
- res.send("Hello world! by express");
-});
-app.get('/test', function(req, res){
- res.send("this is route 2");
-});
+  app.get('/', function(req, res){
+    res.send("Hello world! by express");
+  });
+
+  app.get('/test', function(req, res){
+    res.send("this is route 2");
+  });
+
+  app.get('/add', function(req, res){
+    var x = req.query.x;
+    var y = req.query.y;
+    res.send("X + Y="+(x+y));
+  });
 app.listen(8080);
