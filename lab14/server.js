6bf81ed7722cb2dd91769f147fae25e1cbc69a40
1612673 <acquablue@live.it>
2018-03-20 12:32:13 +0000

    Lab14 part3

--- lab14/server.js
+++ lab14/server.js
@@ -1,7 +1,30 @@
 var express = require('express');
+var params = {screen_name: 'nodejs'};
 var app = express();
  app.use(express.static('public'))
  app.get('/', function(req, res){
+   client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ if (!error) {
+ console.log(tweets);
+ }
+});
+ res.send("Hello world! by express");
+});
+app.listen(8080);
+
+
+var express = require('express');
+var app = express();
+var Twitter = require('twitter');
+var client = new Twitter({
+ consumer_key: 'g98jHP1lHhfofUSYVVEUcYElG',
+ consumer_secret: '6nyKR60Kfb4Bt9q4dGFAwwzrQ9GmA9ydFLWjE3AwUuCpUsi0T7s',
+ access_token_key: '976065842763968513-rQLLQ3jqkkHumyUjZwmbm5j5vxjq8Wy',
+ access_token_secret: '2kVeaY3JmPTM5IDwyyAUNMBYmRKUKqjOhRzYd23gBKQtF'
+});
+app.use(express.static('public'))
+app.get('/'
+, function(req, res){
  res.send("Hello world! by express");
 });
 app.listen(8080);
