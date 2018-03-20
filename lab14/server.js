51ea48a524bfeac18a4b86a19d25b1fded42f5c6
1612673 <acquablue@live.it>
2018-03-20 12:53:31 +0000

    Lab14 part5...?

--- lab14/server.js
+++ lab14/server.js
@@ -21,3 +21,12 @@ else{
 });
 });
 app.listen(8080);
+
+var output = "";
+ for (var t = 0; t < tweets.length; t++) {
+ output += "<div>";
+ output += "<h2>" + tweets[t].user.screen_name + "<h2>";
+ output += "<p>" + tweets[t].text + "</p>"
+ output += "</div>";
+ }
+res.send(output);
